e394667efb4c482ca7585f4e24ffcf14712f9e68
s1540641 <magdlinem1305@gmail.com>
2025-04-26 14:22:36 -0500

    Create playerMovement.cs
    code for movement.

---
+++ My project (1)/Assets/playerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0f, moveZ);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+    }
+}
